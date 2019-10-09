83d1badee587ebc44804394cf9a3ad25b113277e
MD. Masbahul <mesbahul.sohan@gmail.com>
2019-10-09 09:35:03 +0600

    task2

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ heading.innerText = "text";
 
 /* Task 2 using plain JavaScript */
 
+function addText(event){
+    var p = document.createElement("p");
+    p.innerText = "Hello World";
+    var div = document.getElementById('task2a');
+    div.appendChild(p);
+}
+function changeColor(event, color){
+    document.body.style.background = color;
+}
+function GotoGoogle(event){
+    window.location.herf = "https://www.google.com";
+}
+
 
 
 /* Task 4 using jQuery */
